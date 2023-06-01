e4fe2defa782ec44192ddc41c925b5db87b5e07d
Harshad <harshadmanjre@gmail.com>
2023-06-01 09:31:39 +0530

    Text added in sum.java

---
+++ diff.java
@@ -0,0 +1,6 @@
+public class Diff {
+    public static void main(String[] args)
+        int a = 5;
+        int b = 6;
+    System.out.println("The Difference of the two numbers is "a - b);
+    }
--- sum.java
+++ sum.java
@@ -2,5 +2,5 @@ public class Sum {
     public static void main(String[] args)
         int a = 5;
         int b = 6;
-    System.out.println("The Sum is "a+b);
+    System.out.println("The Sum of the two numbers is "a+b);
     }
